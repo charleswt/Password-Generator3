f42ab1a4bf41fb436c4ce65ea8f09bafc8b0a899
ct <charleswtiffany@gmail.com>
2023-11-19 20:28:20 -0800

    added to readme file and added notes

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,46 +1,54 @@
 function generatePassword() {
-    // Prompt for password criteria
-    const length = parseInt(prompt("Enter the password length (between 8 and 128 characters):"));
+
+    // Password criteria prompt
+    var length = parseInt(prompt("Enter the password length (between 8 and 128 characters):"));
 
     if (isNaN(length) || length < 8 || length > 128) {
+
         alert("Please enter a valid password length between 8 and 128 characters.");
+
         return;
     }
 
-    const includeLowercase = confirm("Include lowercase characters?");
-    const includeUppercase = confirm("Include uppercase characters?");
-    const includeNumeric = confirm("Include numeric characters?");
-    const includeSpecial = confirm("Include special characters?");
+    var includeLowercase = confirm("Include lowercase characters?");
+    var includeUppercase = confirm("Include uppercase characters?");
+    var includeNumeric = confirm("Include numeric characters?");
+    var includeSpecial = confirm("Include special characters?");
 
-    // Validate at least one character type is selected
+    // Validates one or more character type
     if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+
         alert("Please select at least one character type.");
+
         return;
     }
 
-    // Generate the password
-    const password = generateRandomPassword(length, includeLowercase, includeUppercase, includeNumeric, includeSpecial);
+    // Generates password
+    var password = generateRandomPassword(length, includeLowercase, includeUppercase, includeNumeric, includeSpecial);
 
-    // Display the password on the page
+    // Displays password on page
     document.getElementById("password").innerText = `${password}`;
 }
 
 function generateRandomPassword(length, includeLowercase, includeUppercase, includeNumeric, includeSpecial) {
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numericChars = '0123456789';
-    const specialChars = '!@#$%^&*()_+{}[]|;:,.<>?';
 
-    let allChars = '';
-    let password = '';
+    // variables
+    var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numericChars = '0123456789';
+    var specialChars = '!@#$%^&*()_+{}[]|;:,.<>?';
+
+    var allChars = '';
+    var password = '';
 
     if (includeLowercase) allChars += lowercaseChars;
     if (includeUppercase) allChars += uppercaseChars;
     if (includeNumeric) allChars += numericChars;
     if (includeSpecial) allChars += specialChars;
 
+    // randomises given text/vars
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * allChars.length);
+        var randomIndex = Math.floor(Math.random() * allChars.length);
         password += allChars.charAt(randomIndex);
     }
 
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# challenge-3
+# Password Generator
+
+This is a random password generator. I feel that I did well on this challenge but I can always improve.
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## About
+
+The purpose of this project was to create a random passowrd generator. During this process, I learned the most js I ever have in just a few sittings. The website now features proper and concise code with no errors, and does as instructed.
+
+## Getting Started
+
+This section is not applicable as it's a description of the project's purpose and improvements.
+
+## Usage
+
+Press "Generate Password", then follow the stemps that are prompted.
+
+## Contributing
+
+This project was developed by [Charles Tiffany](https://github.com/charleswt/).
+
+
+## Screenshot
+
+![screenshot](./Develop/images/sc.png)
+
+## License
+
+Please refer to the LICENSE file in the repository for details.   
+  (./https://github.com/charleswt/challenge-3/blob/main/LICENSE)
